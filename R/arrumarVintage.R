a477ad900c16a8180c956aa3f9a2aa9b194cd92b
guilbran <guilbran@gmail.com>
2017-08-08 19:43:11 -0300

    code in data.frame argument

--- R/arrumarVintage.R
+++ R/arrumarVintage.R
@@ -1,8 +1,8 @@
 #' @title Balanced pannel
 #' @description Create a balanced pannel for data. Select variables, and transform time series to its stationary representation
-#' @param base. data.frame da base de dados que representa a vintage de interesse. Na primeira coluna um vetor de datas e nas demais colunas as variáveis que compõe o vetor.
-#' @param legenda. data.frame contendo uma legenda de qual a transformação necessária para induzir a estacionaridade. O nome das variáveis deve ser 
-#' o mesmo do data.frame base utilizado no primeiro argumento. A transformação utilizada deve ser indicada em uma coluna com o nome de \texttt{transf}.
+#' @param base. \code{data.frame} da base de dados que representa a vintage de interesse. Na primeira coluna um vetor de datas e nas demais colunas as variáveis que compõe o vetor.
+#' @param legenda. \code{data.frame} contendo uma legenda de qual a transformação necessária para induzir a estacionaridade. O nome das variáveis deve ser 
+#' o mesmo do \code{data.frame} utilizado no primeiro argumento. A transformação utilizada deve ser indicada em uma coluna com o nome de \code{transf}.
 #' @import zoo
 #' @import stats
 
