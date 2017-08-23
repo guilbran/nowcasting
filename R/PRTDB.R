dc81edeca80fac6ce4cc871b2812cd44a2d09b9b
guilbran <guilbran@gmail.com>
2017-08-23 12:40:20 -0300

    were to where

--- R/PRTDB.R
+++ R/PRTDB.R
@@ -2,7 +2,7 @@
 #' @description Create a pseudo real time data base, based on delays estipulates by the users, and data inputed by the
 #' @param mts A matrix time series - \code{mts} - with the series to be transformed
 #' @param delay A numeric vector with the delay in days the information is available after the reference month. Each element correspond to the series in the respective column in \code{mts}. 
-#' @param today The day were the data is supposed to be collected.
+#' @param today The day where the data is supposed to be collected.
 #' @return A \code{mts} with the series transformed.
 #' @import zoo lubridate
 #' @export
