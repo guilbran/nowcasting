7697d3d443957e3033888a1996dcc40860c77846
guilbran <guilbran@gmail.com>
2017-09-06 15:18:47 -0300

    Descrip of project

--- R/nowcasting.R
+++ R/nowcasting.R
@@ -1,12 +1,11 @@
 #' @title nowcasting and create Real-Time data basis
 #'
-#' @description Methods and tools for nowcasting Brazilian economic time series. 
-#' It allows extract information in real time, creating a real time data base; 
-#' estimate relationship between macroeconomic variables via estimation of dynamic factors; 
-#' forecast time series in previous periods of reference; 
-#' forecast time series in the current period of reference (nowcasting); 
-#'
-#' The methodology is based on \emph{Giannone, Domenico, Lucrezia Reichlin, and David Small. "Nowcasting: The real-time informational content of macroeconomic data." Journal of Monetary Economics 55.4 (2008): 665-676.}
+#' @description This package is an initiative of the Center for Statistical and Computational Methods (NMEC) belonging to the Brazilian Institute of Economics (IBRE) of the Getúlio Vargas Foundation (FGV). Our goal is to develop a real-time predictive measure of Brazilian GDP that helps IBRE researchers in their analysis about the Brazilian economy.
+#' 
+#' The purpose of this package is to disseminate to the community of \code{R} users the statistical tools and techniques of 'nowcast' already known in the academic literature. One can find in this package a simple and practical way to reproduce several of these techniques for their studies.
+#' 
+#' In this preliminary version of the package we present only one method, based on a seminal article in this literature:
+#' \emph{Giannone, Domenico, Lucrezia Reichlin, and David Small. "Nowcasting: The real-time informational content of macroeconomic data." Journal of Monetary Economics 55.4 (2008): 665-676.}
 #' This is one of the seminal works in the nowcasting area. Some functions are adaptations and translations of this paper's \emph{replication files} avaible in MATLAB and RATS. One can found these \emph{replication files} in the following url:
 #' \url{https://www.newyorkfed.org/research/economists/giannone/pub}
 #'
