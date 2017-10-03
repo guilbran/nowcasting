9eac8f042fca35042f26c4947e9a01ba02858d5a
guilbran <guilbran@gmail.com>
2017-10-03 15:30:33 -0300

    vintage to BRGDP

--- R/USGDP.R
+++ R/USGDP.R
@@ -1,4 +1,4 @@
-#' @name  giannoneetal2008
+#' @name  USGDP
 #' @title Example of replication files in Giannone et al. 2008
 #' @docType data
 #' @format  A \code{list} with 2 elements: 
@@ -8,15 +8,15 @@
 #' \item \code{Legenda} is a \code{data.frame} with specifications of the series in Base.
 #' }
 #' @description Dataset available to replicate the results in \emph{Giannone et al. 2008}. 
-#' @usage giannoneetal2008
+#' @usage USGDP
 #' @source This dataset is available as \emph{replication files} of the seminal work \emph{Giannone 2008}.
 #'  One can find these \emph{replication files} in the following url: \url{https://www.newyorkfed.org/research/economists/giannone/pub}
 #' @references Giannone, D., Reichlin, L., & Small, D. (2008). Nowcasting: The real-time informational content of macroeconomic data. Journal of Monetary Economics, 55(4), 665-676.<doi:10.1016/j.jmoneco.2008.05.010>
 #' @examples 
 #' #### Giannone et al (2008) - Example
-#' trans<-giannoneetal2008$Legenda$Transformation[-length(giannoneetal2008$Legenda$Transformation)]
-#' base<-giannoneetal2008$Base[,-dim(giannoneetal2008$Base)[2]]
-#' gdp<-giannoneetal2008$Base[,dim(giannoneetal2008$Base)[2]]
+#' trans<-USGDP$Legenda$Transformation[-length(USGDP$Legenda$Transformation)]
+#' base<-USGDP$Base[,-dim(USGDP$Base)[2]]
+#' gdp<-USGDP$Base[,dim(USGDP$Base)[2]]
 #' 
 #' # Balanced Panel
 #' base<-Bpanel(base = base,trans = trans)
