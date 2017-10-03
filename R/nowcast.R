345ece5e9906ecbad6780de9b3d9c9906865977f
guilbran <guilbran@gmail.com>
2017-10-03 15:56:53 -0300

    regressors to x

--- R/nowcast.R
+++ R/nowcast.R
@@ -3,7 +3,7 @@
 #' @description Estimate nowcasting and foreacasting for a quarterly time serie. The method is based on 
 #' \emph{Giannone et al. 2008}
 #' @param y Stationary quarterly time-series 
-#' @param regressors A time series matrix (\code{mts}) representing the regressors of interest. The series must be stationary.
+#' @param x A time series matrix (\code{mts}) representing the regressors of interest. The series must be stationary.
 #' @param q Dynamic rank. Number of error terms. If not specified q = 2.
 #' @param r Static rank (r>=q), i.e. number of factors. If not specified r = 2.
 #' @param p AR order of factors. If not specified p = 1.
@@ -16,7 +16,7 @@
 #' @references Giannone, D., Reichlin, L., & Small, D. (2008). Nowcasting: The real-time informational content of macroeconomic data. Journal of Monetary Economics, 55(4), 665-676.<doi:10.1016/j.jmoneco.2008.05.010>
 #' @examples
 #' \dontrun{
-#' pib<-base_extraction(22099)
+#' pib<-base_etraction(22099)
 #' now<-nowcast(lag(pib,-2),Bpanel(vintage,rep(3,dim(vintage)[2])))
 #' now$main
 #' now$factors
@@ -37,16 +37,16 @@
 #' @seealso \code{\link[nowcasting]{base_extraction}}
 #' @export
 
-nowcast <- function(y, regressors, q = 2, r = 2, p = 1,method='giannoneetal2008'){
+nowcast <- function(y, x, q = 2, r = 2, p = 1,method='giannoneetal2008'){
 
   if(method=='giannoneetal2008'){
-    factors <- FactorExtraction(regressors, q = q, r = r, p = p)
+    factors <- FactorExtraction(x, q = q, r = r, p = p)
     fatores <- factors$fator_final
     prev <- bridge(y,fatores)
     # return(list(main = prev$main, reg = prev$reg, factors = factors))
     
   }else if(method=='banrun2011'){
-    factors <- FactorExtraction(regressors, q = q, r = r, p = p)
+    factors <- FactorExtraction(x, q = q, r = r, p = p)
     fatores <- stats::filter(factors$fator_final, c(1,2,3,2,1), sides = 1)
     prev <- bridge(y,fatores)
     
@@ -58,25 +58,25 @@ nowcast <- function(y, regressors, q = 2, r = 2, p = 1,method='giannoneetal2008'
 
   # voltar da padronização
   fit<-factors$fator_final%*%t(factors$eigen$vectors[,1:r])
-  colnames(fit)<-colnames(regressors)
-  x <- regressors
+  colnames(fit)<-colnames(x)
+  x <- x
   z <- x
   s <- apply(z, MARGIN = 2, FUN = sd,na.rm=T)
   M <- apply(z, MARGIN = 2, FUN = mean,na.rm=T)
-  for(i in 1:dim(regressors)[2]){
+  for(i in 1:dim(x)[2]){
     z[,i] <- (x[,i] - M[i])/s[i]
   }
   x1<-fit
-  fore_regressors<-regressors[,colnames(regressors) %in% colnames(fit)]
+  fore_x<-x[,colnames(x) %in% colnames(fit)]
   for(i in colnames(fit)){
     x1[,i]<-s[i]*fit[,i]+M[i]
-    fore_regressors[is.na(fore_regressors[,i]),i] <- x1[is.na(fore_regressors[,i]),i]
+    fore_x[is.na(fore_x[,i]),i] <- x1[is.na(fore_x[,i]),i]
   }
   
   if(!(exists('monthgdp'))){
     monthgdp<-NULL
   }
   
-  return(list(monthgdp=monthgdp,main = prev$main, reg = prev$reg, factors = factors,fore_regressors = fore_regressors))
+  return(list(monthgdp=monthgdp,main = prev$main, reg = prev$reg, factors = factors,fore_x = fore_x))
   
 }
