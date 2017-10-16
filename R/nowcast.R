2d31ec6cf1f6338f64aa81ab2f80fec0eb64122f
guilbran <guilbran@gmail.com>
2017-10-16 19:12:29 -0200

    nowcast with EM
    Introducing new method and rename the previous methods by the estimation approach name

--- R/nowcast.R
+++ R/nowcast.R
@@ -5,7 +5,7 @@
 #' @param q Dynamic rank. Number of error terms. If not specified q = 2.
 #' @param r Static rank (r>=q), i.e. number of factors. If not specified r = 2.
 #' @param p AR order of factors. If not specified p = 1.
-#' @param method GRS2008 for Giannone et al. 2008; BR2011 for Banbura and Runstler 2011;
+#' @param method 2sq: Two stages quarterly for Giannone et al. 2008; 2sm: Two stages monthly for Banbura and Runstler 2011; EM: Expected Maximization Giannone et al 2011
 #' @return A \code{list} containing two elements:
 #' 
 #' A \code{data.frame} named \code{main} contains the original serie, the estimation in the sample, the estimation out of the sample;
@@ -17,23 +17,26 @@
 #' pib<-base_extraction(22099)
 #' retpib2<-month2qtr(diff(diff(lag(pib,-2),3),12))
 #' base1<-Bpanel(BRGDP,rep(4,dim(BRGDP)[2]),aggregate = T)
-#' now1<-nowcast(retpib2,base1,1,1,1,method = 'GRS2008')
+#' now1<-nowcast(retpib2,base1,1,1,1,method = '2sq')
 #'
 #' base2<-Bpanel(BRGDP,rep(4,dim(BRGDP)[2]),aggregate = F)
-#' now2<-nowcast(retpib2,base2,2,2,1,'BR2011')
+#' now2<-nowcast(retpib2,base2,2,2,1,'2sm')
+#' 
+#' now3<-nowcast(retpib2,base2,1,1,1,'EM')
+#' 
 #' }
 #' @seealso \code{\link[nowcasting]{base_extraction}}
 #' @export
 
-nowcast <- function(y, x, q = 2, r = 2, p = 1,method='GRS2008'){
+nowcast <- function(y, x, q = 2, r = 2, p = 1,method='2sq'){
 
-  if(method=='GRS2008'){
+  if(method=='2sq'){
     factors <- FactorExtraction(x, q = q, r = r, p = p)
     fatores <- factors$dynamic_factors
     prev <- bridge(y,fatores)
     # return(list(main = prev$main, reg = prev$reg, factors = factors))
     
-  }else if(method=='BR2011'){
+  }else if(method=='2sm'){
     factors <- FactorExtraction(x, q = q, r = r, p = p)
     fatores <- stats::filter(factors$dynamic_factors, c(1,2,3,2,1), sides = 1)
     prev <- bridge(y,fatores)
@@ -42,6 +45,30 @@ nowcast <- function(y, x, q = 2, r = 2, p = 1,method='GRS2008'){
     monthgdp<-ts(rowSums(aux_month),start=start(factors$dynamic_factors),freq=12)
     
     # return(list(monthgdp=monthgdp,main = prev$main, reg = prev$reg, factors = factors))
+  }else if(method=='EM'){
+    X<-cbind(base1,qtr2month(y))
+    Par<-list(r=rep(r,3),p=p,max_iter=3,i_idio=c(rep(T,dim(x)[2]),F),
+              Rconstr = matrix(c(
+                c(2,3,2,1),
+                c(-1,0,0,0),
+                c(0,-1,0,0),
+                c(0,0,-1,0),
+                c(0,0,0,-1))
+                ,4,5),
+              q = matrix(rep(0,4),4,1),nQ = 1,
+              blocks = matrix(rep(1,dim(x)[2]*3),dim(x)[2],3))
+    Res<-EM_DFM_SS_block_idioQARMA_restrMQ(X,Par)
+    factors<-list(dynamic_factors = Res$FF,A = Res$A, C = Res$C, Q = Res$Q, R = Res$R, initx = Res$Z_0,
+            initV = Res$V_0)
+    monthgdp<-ts(Res$X_sm[,dim(Res$X_sm)[2]],start=start(X),frequency = 12)
+    
+    Y<-cbind(y,month2qtr(monthgdp),month2qtr(monthgdp))
+     Y[is.na(Y[,1]),2]<-NA
+    Y[!is.na(Y[,1]),3]<-NA
+    colnames(Y)<-c('y','in','out')
+    
+    return(list(main = Y,factors = factors,fore_x = ts(Res$X_sm[,-dim(Res$X_sm)[2]],start=start(X),frequency = 12),
+                monthgdp = monthgdp))
   }
 
   # voltar da padronização
