e758058868718a514cd1b24059fe61a94b345e68
guilbran <guilbran@gmail.com>
2017-10-26 11:33:22 -0200

    RTDB, Dai sugestions

--- R/RTDB.R
+++ R/RTDB.R
@@ -14,7 +14,7 @@ RTDB<-function(series_code = NULL,vintage = NULL){
 
   # library(DBI)
   # library(RMySQL)
-  # series_code<-22099
+  # series_code<-666
   # vintage<-as.Date('2017-10-26')
   
   v_ind<-as.character(vintage)
@@ -24,7 +24,7 @@ RTDB<-function(series_code = NULL,vintage = NULL){
   conn = dbConnect(MySQL(),db="pibnow",user="pibnow_user",password="123456",host="200.20.164.178",port=3306)
   dados0 <- DBI::dbGetQuery(conn = conn,statement = SQL)
   DBI::dbDisconnect(conn)
-  dados1<-data.frame(COLUMN_NAMES=dados0[3:(dim(dados0)[1]-1),])
+  dados1<-data.frame(series_code=gsub(dados0[3:(dim(dados0)[1]-1),],'serie',''))
   return(dados1)
   }else{
     return_try<-tryCatch({
@@ -37,7 +37,7 @@ RTDB<-function(series_code = NULL,vintage = NULL){
       })
     
     if(return_try==FALSE){
-    warnings('Sorry, this serie(s) is(are) not available.')
+    message('Sorry, this serie(s) is(are) not available.')
     }else{
       if(nrow(dados0)==0){
         SQL<-paste("SELECT vintage_cod FROM dbvintage")
@@ -47,14 +47,29 @@ RTDB<-function(series_code = NULL,vintage = NULL){
         
         vintage_cod<-as.Date(unique(dados0)[,1])
         if(as.Date(vintage)<vintage_cod[1]){
+          v_ind<-vintage_cod[1]
+          SQL<-paste("SELECT", paste0('X,',paste0("serie",series_code,collapse = ',')) ,"FROM dbvintage WHERE vintage_cod =",paste0("\'",v_ind,"\'"))
+          conn = dbConnect(MySQL(),db="pibnow",user="pibnow_user",password="123456",host="200.20.164.178",port=3306)
+          dados0 <- DBI::dbGetQuery(conn = conn,statement = SQL)
+          DBI::dbDisconnect(conn)
+          dados1<-ts(dados0[,-1],start=as.numeric(c(substr(dados0[1,1],1,4),substr(dados0[1,1],6,7))),frequency=12)
           message(
-            writeLines(c('Sorry, we do not have this vintage for this(ese) serie(s) yet :(',
-                         paste('Try vintages:',vintage_cod[1],':)'))))  
+            writeLines(c('Sorry, this vintage is not available for this(ese) serie(s) yet :(',
+                         paste('But, I return (invisible) the first vintage values:',vintage_cod[1],':)'))))
+          invisible(dados1)
+          
         }else{
         ind_previous<-max(which(vintage_cod<as.Date(vintage)))
+        v_ind<-vintage_cod[ind_previous]
+        SQL<-paste("SELECT", paste0('X,',paste0("serie",series_code,collapse = ',')) ,"FROM dbvintage WHERE vintage_cod =",paste0("\'",v_ind,"\'"))
+        conn = dbConnect(MySQL(),db="pibnow",user="pibnow_user",password="123456",host="200.20.164.178",port=3306)
+        dados0 <- DBI::dbGetQuery(conn = conn,statement = SQL)
+        DBI::dbDisconnect(conn)
+        dados1<-ts(dados0[,-1],start=as.numeric(c(substr(dados0[1,1],1,4),substr(dados0[1,1],6,7))),frequency=12)
         message(
-          writeLines(c('Sorry, we do not have this vintage for this(ese) serie(s) yet :(',
-                       paste('Try vintages:',vintage_cod[ind_previous],'or',vintage_cod[ind_previous+1],':)'))))
+          writeLines(c('Sorry, this vintage is not available for this(ese) serie(s) yet :(',
+                       paste('But, I return (invisible) the last vintage available:',vintage_cod[ind_previous],':)'))))
+        invisible(dados1)
         }
       }else{
           dados1<-ts(dados0[,-1],start=as.numeric(c(substr(dados0[1,1],1,4),substr(dados0[1,1],6,7))),frequency=12)
